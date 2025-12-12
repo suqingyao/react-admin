03039823781b47c888de8dc874cc03e1702939f2
suqingyao <suqingyao333@gmail.com>
2025-12-12 10:52:58 +0800

    feat(nova-components): add onClick prop to NovaSvgIcon and NovaLogo

--- src/components/core/base/nova-logo/index.tsx
+++ src/components/core/base/nova-logo/index.tsx
@@ -2,12 +2,19 @@ import { cn } from '@suqingyao/utils';
 
 import { NovaSvgIcon } from '../nova-svg-icon';
 
-export function NovaLogo({ className, style }: { className?: string; style?: React.CSSProperties }) {
+interface NovaLogoProps {
+  className?: string;
+  style?: React.CSSProperties;
+  onClick?: () => void;
+}
+
+export function NovaLogo({ className, style, onClick }: NovaLogoProps) {
   return (
     <NovaSvgIcon
       icon="ri:star-s-fill"
       className={cn('w-8 h-8', className)}
       style={style}
+      onClick={onClick}
     />
   );
 }
--- src/components/core/base/nova-svg-icon/index.tsx
+++ src/components/core/base/nova-svg-icon/index.tsx
@@ -5,8 +5,9 @@ interface NovaSvgIconProps {
   icon: string;
   className?: string;
   style?: React.CSSProperties;
+  onClick?: () => void;
 }
 
-export function NovaSvgIcon({ icon, className, style }: NovaSvgIconProps) {
-  return <Icon icon={icon} className={cn('nova-svg-icon inline', className)} style={style} />;
+export function NovaSvgIcon({ icon, className, style, onClick }: NovaSvgIconProps) {
+  return <Icon icon={icon} className={cn('nova-svg-icon inline', className)} style={style} onClick={onClick} />;
 }
