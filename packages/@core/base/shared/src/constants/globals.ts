9d6aaa44753eeb1e5ac70e4138ce0bddab4123a4
suqingyao <suqingyao333@gmail.com>
2026-01-04 20:18:47 +0800

    feat(shared): add core shared utilities and components

---
+++ packages/@core/base/shared/src/constants/globals.ts
@@ -0,0 +1,16 @@
+/** layout content 组件的高度 */
+export const CSS_VARIABLE_LAYOUT_CONTENT_HEIGHT = `--nova-content-height`;
+/** layout content 组件的宽度 */
+export const CSS_VARIABLE_LAYOUT_CONTENT_WIDTH = `--nova-content-width`;
+/** layout header 组件的高度 */
+export const CSS_VARIABLE_LAYOUT_HEADER_HEIGHT = `--nova-header-height`;
+/** layout footer 组件的高度 */
+export const CSS_VARIABLE_LAYOUT_FOOTER_HEIGHT = `--nova-footer-height`;
+
+/** 内容区域的组件ID */
+export const ELEMENT_ID_MAIN_CONTENT = `__vben_main_content`;
+
+/**
+ * @zh_CN 默认命名空间
+ */
+export const DEFAULT_NAMESPACE = 'nova';
---
+++ packages/@core/base/shared/src/constants/vben.ts
@@ -0,0 +1,27 @@
+/**
+ * @zh_CN GITHUB 仓库地址
+ */
+export const VBEN_GITHUB_URL = 'https://github.com/vbenjs/vue-nova-admin';
+
+/**
+ * @zh_CN 文档地址
+ */
+export const VBEN_DOC_URL = 'https://doc.nova.pro';
+
+/**
+ * @zh_CN Nova Logo
+ */
+export const VBEN_LOGO_URL = 'https://unpkg.com/@vbenjs/static-source@0.1.7/source/logo-v1.webp';
+
+/**
+ * @zh_CN Nova Admin 首页地址
+ */
+export const VBEN_PREVIEW_URL = 'https://www.nova.pro';
+
+export const VBEN_ELE_PREVIEW_URL = 'https://ele.nova.pro';
+
+export const VBEN_NAIVE_PREVIEW_URL = 'https://naive.nova.pro';
+
+export const VBEN_ANT_PREVIEW_URL = 'https://ant.nova.pro';
+
+export const VBEN_TD_PREVIEW_URL = 'https://tdesign.nova.pro';
